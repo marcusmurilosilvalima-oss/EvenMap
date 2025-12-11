3df8fa75e7c6b4cd384e64b9c20e738063448e26
marcusmurilosilvalima-oss <marcusmurilosilvalima@gmail.com>
2025-12-10 22:03:53 -0300

    Add files via upload
    código bruto

---
+++ evenMap_proto/app.js
@@ -0,0 +1,9 @@
+function login() {
+    window.location.href = "home.html";
+}
+function cadastro() {
+    window.location.href = "cadastro.html";
+}
+function criarConta() {
+    window.location.href = "home.html";
+}
